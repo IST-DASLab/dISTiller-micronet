0d8565602118944a7a8e3f8308e808c9699e1b1a
alexturn <alexmtcore@gmail.com>
2019-07-09 14:21:27 +0200

    add different load options

--- imagenet_efficientnet/import_net.py
+++ imagenet_efficientnet/import_net.py
@@ -6,7 +6,11 @@ from distiller.models import create_model
 from distiller.apputils import load_checkpoint
 
 
-__all__ = ['get_masked_model']
+__all__ = ['get_masked_model_from_distiller_checkpoint',
+           'get_masked_model_from_quantdistil_checkpoint']
+
+TEST_CHECKPOINT = '/nfs/scistore08/alistgrp/ashevche/distiller-data/checkpoints/effnet_imagenet_prune_base2\
+___2019.07.07-231317/effnet_imagenet_prune_base2_checkpoint.pth.tar'
 
 
 class MaskedNet(nn.Module):
@@ -18,6 +22,7 @@ class MaskedNet(nn.Module):
 
         if maskers is not None:
             self.masks = {name: maskers[name].mask for name in maskers.keys()}
+            self.register_hooks()
 
     def forward(self, in_tensor):
         return self.net(in_tensor)
@@ -27,7 +32,9 @@ class MaskedNet(nn.Module):
             raise ValueError('Please remove previous hooks before new created')
         if self.masks is None:
             raise ValueError('The are no masks provided in the __init__')
-        for name, param in self.net.named_parameters:
+
+        self.hooks = []
+        for name, param in self.net.named_parameters():
             self.hooks.append(
                 param.register_hook(self._build_masking_hook(self.masks[name]))
             )
@@ -43,12 +50,20 @@ class MaskedNet(nn.Module):
         hook = lambda grad: grad * mask
         return hook
 
-def get_masked_model(checkpoint):
+def get_masked_model_from_distiller_checkpoint(checkpoint):
     model = create_model(False, 'imagenet', 'efficientnetb1')
     model, compression_scheduler, optimizer, start_epoch = apputils.load_checkpoint(model, checkpoint)
     masked_model = MaskedNet(model, compression_scheduler.zeros_mask_dict)
     return masked_model
 
+def get_masked_model_from_quantdistil_checkpoint(model, distiller_checkpoint=None):
+    maskers = None
+    if distiller_checkpoint is not None:
+        maskers = apputils.load_checkpoint(model, checkpoint)[1].zeros_mask_dict
+    masked_model = MaskedNet(model, maskers)
+    return masked_model
+
 
 if __name__ == '__main__':
-    pass
+    masked_net = get_masked_model_from_distiller_checpoint(TEST_CHECKPOINT)
+    print(masked_net.state_dict())
