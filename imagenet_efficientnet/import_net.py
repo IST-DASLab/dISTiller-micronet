0ec5dac0c347a3c73d7dfb33ecb41d22d3ae28df
alexturn <alexmtcore@gmail.com>
2019-07-09 13:16:23 +0200

    hook for grad mask

--- imagenet_efficientnet/import_net.py
+++ imagenet_efficientnet/import_net.py
@@ -6,37 +6,30 @@ from distiller.models import create_model
 from distiller.apputils import load_checkpoint
 
 
+__all__ = ['get_masked_model']
 
 TEST_CHECKPOINT = '/nfs/scistore08/alistgrp/ashevche/distiller-data/checkpoints/effnet_imagenet_prune_base2\
 ___2019.07.07-231317/effnet_imagenet_prune_base2_checkpoint.pth.tar'
 
-"""
-TODO: Think if copies are needed if we start training from
-scratch even if optimizer has momentum like property.
-"""
+
 class MaskedNet(nn.Module):
     def __init__(self, net, masks):
         super(MaskedNet, self).__init__()
         self.net = net
+        self.masks = dict()
 
-        self.copy, self.masks = dict(), dict()
         for name, param in self.net.named_parameters():
             if name in masks.keys():
-                self.copy[name] = param.data.clone().detach()
                 self.masks[name] = (param.data != 0).float()
+                param.register_hook(self._build_masking_hook(self.masks[name]))
 
     def forward(self, in_tensor):
-        self.copy = dict()
-        for name, param in self.net.named_parameters():
-            if name in self.masks.keys():
-                param.data *= self.masks[name]
         return self.net(in_tensor)
 
-    def revert_pruned(self):
-        for name, param in self.net.named_parameters():
-            if name in self.masks.keys():
-                mask = ~self.masks[name].byte()
-                param.data[mask] = self.copy[name][mask]
+    @staticmethod
+    def _build_masking_hook(mask):
+        hook = lambda grad: grad * mask
+        return hook
 
 def get_masked_model(checkpoint):
     model = create_model(False, 'imagenet', 'efficientnetb1')
@@ -45,11 +38,11 @@ def get_masked_model(checkpoint):
     return masked_model
 
 
-
 if __name__ == '__main__':
     masked_net = get_masked_model(TEST_CHECKPOINT)
-    in_tensor = torch.zeros(1, 3, 224, 224)
-    masked_net(in_tensor)
+    in_tensor = torch.ones(1, 3, 224, 224)
+    loss = nn.CrossEntropyLoss()(masked_net(in_tensor), torch.ones(1).long().cuda())
+    loss.backward()
     for name, param in masked_net.net.named_parameters():
         if name in masked_net.masks.keys():
-            print((param == 0).sum())
+            print((param.grad.data == 0).sum())
