005a279978d096a8fb116c70b308ad0be8930a50
alexturn <alexmtcore@gmail.com>
2019-07-10 18:27:37 +0200

    add script + schedule for quantization

---
+++ imagenet_efficientnet/scripts/train_quantized.sh
@@ -0,0 +1,28 @@
+EXP_NAME="effnet_imagenet_train_quantized"
+CHECKPOINT_ROOT="/nfs/scistore08/alistgrp/ashevche/distiller-data/checkpoints"
+ARCH="efficientnetb1"
+DATAPATH="/nfs/scistore08/alistgrp/ashevche/distiller-data/data.imagenet"
+CHECKPOINT="/nfs/scistore08/alistgrp/ashevche/distiller-data/checkpoints/effnet_imagenet_prune_base2___2019.07.07-231317/\
+effnet_imagenet_prune_base2_checkpoint.pth.tar"
+
+LR=0.0001
+MOM=0.9
+WD=0.0
+
+python compress_classifier.py \
+	--name=$EXP_NAME \
+	--arch=$ARCH $DATAPATH \
+	--epochs=300 -p=30 -b=256 -j=30 \
+	--lr=$LR --momentum=$MOM --wd=$WD \
+	--pretrained \
+	--vs=0 \
+	--out-dir=$CHECKPOINT_ROOT \
+	--resume-from=$CHECKPOINT \
+	--gpus 3,4,5,6,7 \
+	--reset-optimizer \
+	--compress=./schedules/quantize_aware_training.yaml \
+	--kd-teacher='efficientnetb2' \
+	--kd-pretrained \
+	--kd-temperature=2 \
+	--kd-student-wt=0.3 \
+	--kd-start-epoch=0 \
