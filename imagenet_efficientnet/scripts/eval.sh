ad7a9a9c9dc237898cc2a64677b635d755874359
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-07-24 12:22:25 +0000

    V1 migration

--- imagenet_efficientnet/scripts/eval.sh
+++ imagenet_efficientnet/scripts/eval.sh
@@ -1,20 +1,19 @@
 # Call to evaluate pretrained weights
 
-EXP_NAME="effnet_imagenet_eval_initial_weights"
-CHECKPOINT_ROOT="/nfs/scistore08/alistgrp/ashevche/distiller-data/checkpoints"
+EXP_NAME="effnet_imagenet_eval"
+CHECKPOINT_ROOT="/home/alex/checkpoints"
 ARCH="efficientnetb1"
-DATAPATH="/home/imarkov/Datasets/imagenet"
-CHECKPOINT="/nfs/scistore08/alistgrp/ashevche/distiller-data/checkpoints/\
-effnet_imagenet_prune_base2___2019.07.21-210409/effnet_imagenet_prune_base2_best.pth.tar"
+DATAPATH="/home/markovilya197/Datasets/imagenet"
+CHECKPOINT="/home/alex/checkpoints/effnet_imagenet_train_quantized___2019.07.23-195312/effnet_imagenet_train_quantized_best.pth.tar"
  
 python compress_classifier.py \
 	--name=$EXP_NAME \
 	--arch=$ARCH $DATAPATH \
 	--pretrained \
 	--resume-from=$CHECKPOINT \
-	--gpus 0 \
+	--gpus 2 \
 	-j=5 \
 	--evaluate \
 	#--quantize-eval \
-	#--qe-config-file=./schedules/quantize.yaml
+	# --qe-config-file=./schedules/quantize.yaml
 	#--summary="sparsity"
